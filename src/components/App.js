507238bb184bb642b623c830dd6f231bc0495279
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-09-30 23:20:40 +0300

    CRUD code along

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((quiz) => {
+        setQuestions(quiz);
+      });
+  }, [])
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      const updatedQuestions = questions.filter((question) => question.id !== id);
+      setQuestions(updatedQuestions);
+    });
+  }
+
+  function handleAddQuestion(newQuestion) {
+    const updatedQuestions = [...questions, newQuestion];
+    setQuestions(updatedQuestions);
+
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm handleAddQuestion={handleAddQuestion}/> : <QuestionList questions={questions} handeDelete={handleDelete}/>}
     </main>
   );
 }
